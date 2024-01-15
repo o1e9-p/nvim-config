1a0c9866812b576af46255cee7597a2f6cb9dc59
Oleg Polyakov <o.polyakov@cryptology.com>
2024-01-15 15:42:43 +0300

    fix

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -50,8 +50,8 @@ require('mason-lspconfig').setup({
         cmd = {'golangci-lint-langserver'},
 		  	root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
 			  init_options = {
-					-- command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json" };
-				  command = { "golangci-lint", "run","--enable-all", "--disable", "varnamelen,nosnakecase,nonamedreturns,wrapcheck", "--out-format", "json" };
+					command = { "golangci-lint", "run",  "--out-format", "json" };
+				  -- command = { "golangci-lint", "run","--enable-all", "--disable", "varnamelen,nosnakecase,nonamedreturns,wrapcheck", "--out-format", "json" };
 			  },
         filetypes = {'go', 'gomod'},
       })
