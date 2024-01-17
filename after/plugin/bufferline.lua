8e18a5e6b219b9f464afc224e91ec1fac6f5b091
Oleg Polyakov <o.polyakov@cryptology.com>
2024-01-17 16:36:59 +0300

    fix remap

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -5,3 +5,18 @@ require("bufferline").setup({
   },
 })
 
+vim.keymap.set('n', '<right>', ':BufferLineMoveNext<CR>')
+vim.keymap.set('n', '<left>', ':BufferLineMovePrev<CR>')
+
+-- go to tab (Not working)
+vim.keymap.set('n', '<leader>1', 'lua require"bufferline".go_to(1)<CR>')
+vim.keymap.set('n', '<leader>2', 'lua require"bufferline".go_to(2)<CR>')
+vim.keymap.set('n', '<leader>3', 'lua require"bufferline".go_to(3)<CR>')
+vim.keymap.set('n', '<leader>4', 'lua require"bufferline".go_to(4)<CR>')
+vim.keymap.set('n', '<leader>5', 'lua require"bufferline".go_to(5)<CR>')
+vim.keymap.set('n', '<leader>6', 'lua require"bufferline".go_to(6)<CR>')
+vim.keymap.set('n', '<leader>7', 'lua require"bufferline".go_to(7)<CR>')
+vim.keymap.set('n', '<leader>8', 'lua require"bufferline".go_to(8)<CR>')
+vim.keymap.set('n', '<leader>9', 'lua require"bufferline".go_to(9)<CR>')
+vim.keymap.set('n', '<leader>$', 'lua require"bufferline".go_to( -1)<CR>')
+
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -49,17 +49,4 @@ vim.keymap.set('n', '<Esc><Esc>', ':only<CR>')
 -- close current tab
 vim.keymap.set('n', 'bd', ':bd<CR>lua require"bufferline".go_to(num)<CR>')
 
--- go to tab (Not working)
-vim.keymap.set('n', '<leader>1', 'lua require"bufferline".go_to(1)<CR>')
-vim.keymap.set('n', '<leader>2', 'lua require"bufferline".go_to(2)<CR>')
-vim.keymap.set('n', '<leader>3', 'lua require"bufferline".go_to(3)<CR>')
-vim.keymap.set('n', '<leader>4', 'lua require"bufferline".go_to(4)<CR>')
-vim.keymap.set('n', '<leader>5', 'lua require"bufferline".go_to(5)<CR>')
-vim.keymap.set('n', '<leader>6', 'lua require"bufferline".go_to(6)<CR>')
-vim.keymap.set('n', '<leader>7', 'lua require"bufferline".go_to(7)<CR>')
-vim.keymap.set('n', '<leader>8', 'lua require"bufferline".go_to(8)<CR>')
-vim.keymap.set('n', '<leader>9', 'lua require"bufferline".go_to(9)<CR>')
-vim.keymap.set('n', '<leader>$', 'lua require"bufferline".go_to( -1)<CR>')
-
-vim.keymap.set('n', 'pn', 'o<Esc>p')
 vim.keymap.set('n', 'pn', 'o<Esc>p')
